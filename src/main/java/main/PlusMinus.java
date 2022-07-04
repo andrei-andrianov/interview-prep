a0c1f61ba201baf5eef7c4b6126ff9d51be8277d
Andrei Andrianov <andrei.andrianov42@gmail.com>
2022-07-04 14:54:11 +0200

    quick sort

--- src/main/java/main/PlusMinus.java
+++ src/main/java/main/PlusMinus.java
@@ -8,13 +8,9 @@ import java.util.List;
 public class PlusMinus {
 
     public static void main(String[] args) {
-        System.out.println(Result.diagonalDifference(
-                List.of(
-                    List.of(11,2,4),
-                    List.of(4,5,6),
-                    List.of(10,8,-12)
-                )
-        ));
+        int[] arr = {4,5,42,1,97,14,5,6,0};
+        Result.quickSort(arr, 0, 8);
+        Arrays.stream(arr).forEach(System.out::println);
     }
 
 }
@@ -103,6 +99,36 @@ class Result {
         }
 
         return Arrays.stream(array).boxed().toList();
+    }
+
+    public static void quickSort(int[] arr, int begin, int end){
+        if (begin < end) {
+            int partitionIndex = partition(arr, begin, end);
+
+            quickSort(arr, begin, partitionIndex-1);
+            quickSort(arr, partitionIndex+1, end);
+        }
+    }
+
+    private static int partition(int[] arr, int begin, int end) {
+        int pivot = arr[end];
+        int i = (begin-1);
+
+        for (int j = begin; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+
+                int swapTemp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = swapTemp;
+            }
+        }
+
+        int swapTemp = arr[i+1];
+        arr[i+1] = arr[end];
+        arr[end] = swapTemp;
+
+        return i+1;
     }
 
 }
